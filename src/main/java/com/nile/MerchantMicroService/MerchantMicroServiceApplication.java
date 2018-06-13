7b7386ab061a81789df1d1936ef021291f60f628
Dip Halani <dip.thakkar777@gmail.com>
2018-06-13 11:26:49 +0530

    first commit

---
+++ src/main/java/com/nile/MerchantMicroService/MerchantMicroServiceApplication.java
@@ -0,0 +1,12 @@
+package com.nile.MerchantMicroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantMicroServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nile/MerchantMicroService/MerchantMicroServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nile.MerchantMicroService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MerchantMicroServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
